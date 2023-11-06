6242e3b32a8b5cf3a314423ef27822c4a03bca4b
tjalsgur0083 <tjalsgur0083@gmail.com>
2023-11-06 10:39:09 +0900

    image picker

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:coco_music_app/page/start_page.dart';
+import 'package:coco_music_app/page/select_page.dart';
+
+void main() {
+  //WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  //FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Music App',
+      //theme
+      getPages: [
+        GetPage(
+        name: '/select',
+        page: () => const SelectPage()
+        ),
+      ],
+      home: const StartPage()
+    );
+  }
+}
---
+++ lib/page/select_page.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+// import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class SelectPage extends StatefulWidget {
+  const SelectPage({super.key});
+
+  @override
+  State<SelectPage> createState() => _SelectPageState();
+}
+
+class _SelectPageState extends State<SelectPage> {
+  final ImagePicker picker = ImagePicker();
+  XFile? image;
+  final List<XFile> multiImage = [];
+  List<XFile>? images = [];
+  
+
+  // void getMultiImage(ImageSource imageSource) async {
+  //   final List<XFile>? images = await _picker.pickMultiImage();
+  //   if (images != null) {
+  //     setState(() {
+  //       _pickedImage.addAll(images);
+  //     });
+  //   }
+  // }
+  
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: screenHeight * 0.13),
+              // _area(),
+              SizedBox(height: screenHeight * 0.3),
+              bodyWidget()
+            ],
+          )
+        )
+      ),
+    );
+  }
+
+  Widget _area() {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        shrinkWrap: true,
+        itemCount: images!.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1/1,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+
+        itemBuilder: (BuildContext context, int index){
+          return Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,   //사진 크기를 Container 크기에 맞춤
+                    image: FileImage(File(images![index].path))
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      ),
+    );
+  }
+  
+  Widget bodyWidget () {
+    return Column(
+      children: [
+        _cameraButton(),
+          const SizedBox(height: 10),
+          _photoButton(),
+      ],
+    );
+  }
+
+  Widget _cameraButton() {
+    return Column(
+      children: [
+        FloatingActionButton(
+          onPressed: () async {
+           final image = await picker.pickImage(source: ImageSource.camera);
+           if (image != null) {
+             setState(() {
+               images!.add(image);
+             });
+           }
+          },
+          heroTag: 'image0',
+          child: const Icon(Icons.camera),
+        ),
+      ],
+    );
+  }
+  
+  Widget _photoButton() {
+    return Column(
+      children: [
+        FloatingActionButton(
+          onPressed: () async {
+            final multiImage = await picker.pickMultiImage();
+            setState(() {
+              images!.addAll(multiImage);
+            });
+          },
+          heroTag: 'image1',
+          child: const Icon(Icons.photo_album),
+        ),
+      ],
+    );
+  }
+}
